e99f31e3e021464446c95e0391a6319931a7f93c
Tobias Franz <cinex.mail@web.de>
2021-12-11 13:16:37 +0100

    Initial working version with a proper README
    Signed-off-by: Tobias Franz <cinex.mail@web.de>

--- scripts/PerspectiveTokenScaler.js
+++ scripts/PerspectiveTokenScaler.js
@@ -1,23 +1,22 @@
 export class PerspectiveTokenScaler {
 
     static handleUpdate(token, change) {
-        if (token instanceof TokenDocument && change.x && change.y) {
+        if (token instanceof TokenDocument && (change.x || change.y)) {
             let currentScene = game.scenes.viewed
-            let starterData = currentScene.drawings.filter(d => d.data.text === 'starter')?.[0]?.data
-            let limiterData = currentScene.drawings.filter(d => d.data.text === 'limiter')?.[0]?.data
-            let maxDistance = 0
+            let starterData = currentScene.drawings.filter(d => d.data.text === 'scaler_start')?.[0]?.data
+            let limiterData = currentScene.drawings.filter(d => d.data.text === 'scaler_limit')?.[0]?.data
 
             if (typeof starterData !== 'undefined' && typeof limiterData !== 'undefined') {
-                let xDiff = limiterData.x - starterData.x
-                let yDiff = limiterData.y - starterData.y
-                maxDistance = Math.sqrt(xDiff * xDiff + yDiff * yDiff)
-            }
+                let xDiffMax = limiterData.x - starterData.x
+                let yDiffMax = limiterData.y - starterData.y
+                let maxDistance = Math.sqrt(xDiffMax * xDiffMax + yDiffMax * yDiffMax)
 
-            let xDiff = limiterData.x - change.x
-            let yDiff = limiterData.y - change.y
-            let distance = Math.sqrt(xDiff * xDiff + yDiff * yDiff)
-            let factor = distance > maxDistance ? 1 : distance / maxDistance
-            change.scale = factor < 0.25 ? 0.25 : factor
+                let xDiffCurrent = limiterData.x - (change.x ? change.x : 0)
+                let yDiffCurrent = limiterData.y - (change.y ? change.y : 0)
+                let distance = Math.sqrt(xDiffCurrent * xDiffCurrent + yDiffCurrent * yDiffCurrent)
+                let factor = distance > maxDistance ? 1 : distance / maxDistance
+                change.scale = factor < 0.25 ? 0.25 : factor
+            }
         }
     }
 }
